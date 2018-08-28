508a894c07d1e553f890d131c7d349320f321c3d
rishabhc32 <rishabhc32@gmail.com>
2018-08-28 17:18:33 +0530

    Initial Commit

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    home: true,
+    title: 'Hello RC',
+    description: 'Just playing around',
+    themeConfig: {
+        lastUpdated: 'Last Updated',
+    },
+    footer: 'MIT Licensed | Copyright © 2018-present Evan You'
+}
